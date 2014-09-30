38d5fa085d0ed673dfeb75273c4772202660d044
Vladimir <sirvantos@gmail.com>
2014-10-01 01:10:51 +0400

    Added post mark settings

--- app/mailers/base_mailer.rb
+++ app/mailers/base_mailer.rb
@@ -1,4 +1,4 @@
 class BaseMailer < ActionMailer::Base
-	default from: ENV['MAIL_FROM'] || "from@example.com"
+	default from: ENV['MAIL_FROM'] || "noreply@companymarket.com"
   default template_path: 'shared/mailer'
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,4 +79,7 @@ CompanyMarket::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  config.action_mailer.delivery_method   = :postmark
+  config.action_mailer.postmark_settings = { :api_key => ENV['POSTMARK_API_KEY'] }
 end
